e0eff47b8e8eaf6b1adb798c5231818a8e6b096a
Jozef Inglot <inglotj@rferl.org>
2019-12-19 11:43:20 +0100

    work on first feature branch

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Development of First Feature
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"

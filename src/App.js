f7431eb9f4a9fe2d3644cea49cf12c1c5a8f77c7
Jozef Inglot <inglotj@rferl.org>
2019-12-19 11:46:00 +0100

    work on first feature branch finished

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Development of First Feature, continuing
+          Edit <code>src/App.js</code> and save to reload. 
+		  Development of First Feature, finished
         </p>
 
         <a
